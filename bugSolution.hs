c78fabae3bd854415e89c7384e76e9211f0409b0
BugHunter-X <askzenai@gmail.com>
2025-02-20 07:32:04 +0100

    Add bugSolution.hs file

---
+++ bugSolution.hs
@@ -0,0 +1,10 @@
+```haskell
+import Data.List (sort)
+
+main :: IO ()
+main = do
+  let xs = [1, 2, 3, 4, 5]
+  let ys = sort xs
+  print ys -- Correctly prints the sorted list
+  print xs -- Prints the original list, unchanged
+```
